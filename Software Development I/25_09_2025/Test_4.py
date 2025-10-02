eedd91aebab359bde72aaf584b125885ef4b416c
rodkevproxy <rodkevproxy@gmail.com>
2025-10-02 10:42:32 +0100

    Added the final test

--- Software Development I/25_09_2025/Test_4.py
+++ Software Development I/25_09_2025/Test_4.py
@@ -1,11 +1,11 @@
 #Fixing code 
 
-height_1_m=3 
-height_2_m=2 
+height_1_m = 3 
+height_2_m = 2 
 
-#Adding a variable for Pi is not really nnecesary 
+#Adding a variable for Pi is not really necesary just write down the value or math.py  
 
-Volume_In_Cubic_Metres=3.1416 * (height_2_m)**2 * height_1_m / 3 
+Volume_In_Cubic_Metres = 3.1416 * (height_2_m)**2 * height_1_m / 3 
 
 Volume_In_Litres = Volume_In_Cubic_Metres * 1000 
 
